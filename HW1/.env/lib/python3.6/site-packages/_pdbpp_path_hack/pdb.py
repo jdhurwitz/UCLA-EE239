427e8e9ec06b22cdea36104ddce6b3b5e8016ef5
jdhurwitz <jdhurwitz@ucla.edu>
2018-01-21 01:27:06 -0800

    removing my env because I'm dumb

--- HW1/.env/lib/python3.6/site-packages/_pdbpp_path_hack/pdb.py
+++
@@ -1,5 +0,0 @@
-# this file is needed to hijack pdb without eggs
-import os.path
-exec(compile(open(os.path.join(
-    os.path.dirname(os.path.dirname(__file__)), 'pdb.py')).read(), os.path.join(
-    os.path.dirname(os.path.dirname(__file__)), 'pdb.py'), 'exec'))
